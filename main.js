68a671d5f2039e451e6b500c2ee0caf03f2face2
Luke Beretta <skyflea@gmail.com>
2026-03-09 13:39:12 +0200

    Make nav transparent initially on scroll

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ const observer = new IntersectionObserver((entries) => {
 }, { threshold: 0.07, rootMargin: '0px 0px -30px 0px' });
 
 document.querySelectorAll('.fade-in').forEach(el => observer.observe(el));
+
+const nav = document.querySelector('nav');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
